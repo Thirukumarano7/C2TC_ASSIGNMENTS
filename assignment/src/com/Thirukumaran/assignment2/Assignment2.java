e54b15a438665ecc38db9e11778ecbe5a77304e0
thiru <thiru@Thirukumaran>
2025-10-04 10:41:42 +0530

    assignment 2

---
+++ assignment/src/com/Thirukumaran/assignment2/Assignment2.java
@@ -0,0 +1,15 @@
+package com.Thirukumaran.assignment2;
+
+public class Assignment2 {
+	 public static void main(String[] args) {
+	        // Create Student object (default constructor runs automatically)
+	        Student s1 = new Student();
+
+	        // Create Commission object and call methods
+	        Commission emp = new Commission();
+	        emp.acceptDetails();
+	        emp.calculateCommission();
+	    }
+	
+
+}
